ea550eb037fa09587cb29c6432c84a92288436ef
muscode101 <juliousdeveloer@gmail.com>
2021-01-30 18:03:20 +0200

    prep before deploy

--- server.js
+++ server.js
@@ -5,8 +5,7 @@ const connectDB = require('./config/db')
 
 const app = express()
 app.use(cors())
-// const PORT = process.env.port || 5000
-const PORT = process.env.port
+const PORT = process.env.PORT || 5000
 
 connectDB()
 app.use(express.json())

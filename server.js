b0bffaf34619bcf2b5958334ccdfb8c56f7bd296
muscode101 <juliousdeveloer@gmail.com>
2021-01-30 17:31:32 +0200

    prep before deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.listen(PORT, () => console.log(`sever started on port ${PORT} `))
 function serveStaticAsserts() {
     if (process.env.NODE_ENV === 'production') {
         app.use(express.static('client/build'))
-        app.get('*', (req, res) => {
+        app.get('/', (req, res) => {
             res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
         })
     }
